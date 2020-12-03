bb2735c7508b29261939a24bdef7fecb0cced3c8
mayank-bh22 <mayank.rewari1997@gmail.com>
2020-12-03 15:56:25 +0530

    second commit

---
+++ WebApplication1/WebApplication1/Models/Emp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Emp
+    {
+       public int EId {get;set;}
+        public string EName { get; set; }
+        public string EDesig { get; set; }
+        public DateTime EDOJ { get; set; }
+
+    }
+}
